501f1cde7bf2895d7fb159929c4334470dd66b91
sagar <sagar.sahay@spektrix.com>
2020-04-04 19:10:29 +0100

    word ladder done

--- WordLadder/Solution.cs
+++ WordLadder/Solution.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Threading;
 
 namespace WordLadder
 {
@@ -11,20 +13,57 @@ namespace WordLadder
         {
             var solution = new List<string>();
             var res = new List<List<string>>();
-            var neighbours = new Dictionary<string,List<string>>();
-            var distance  = new Dictionary<string, int>();
+            var neighbours = new Dictionary<string, List<string>>();
+            var distance = new Dictionary<string, int>();
             var dict = new HashSet<string>(wordList);
+            dict.Add(beginWord);
 
             foreach (var word in wordList)
             {
                 neighbours.Add(word, new List<string>());
             }
+            neighbours.Add(beginWord, new List<string>());
 
             bfs(beginWord, endWord, dict, neighbours, distance);
-            
+            dfs(beginWord, endWord, dict, neighbours, distance, solution, res);
+            res.OrderBy(x => x.Count);
+            if (res.Count > 0)
+            {
+                return res[0].Count;
+            }
+
+            return 0;
         }
 
-        private void bfs(string beginWord, string endWord, HashSet<string> dict, Dictionary<string,List<string>> neighbours, Dictionary<string,int> distance)
+        private void dfs(
+            string beginWord,
+            string endWord,
+            HashSet<string> dict,
+            Dictionary<string, List<string>> neighbours,
+            Dictionary<string, int> distance,
+            List<string> solution,
+            List<List<string>> res)
+        {
+           solution.Add(beginWord);
+           if (beginWord == endWord)
+           {
+               res.Add(new List<string>(solution));
+           }
+           else
+           {
+               foreach (var neighbour in neighbours[beginWord])
+               {
+                   if (distance[neighbour] == distance[beginWord] + 1)
+                   {
+                       dfs(neighbour, endWord, dict,neighbours,distance,solution,res);
+                   }
+               }
+           }
+           solution.RemoveAt(solution.Count - 1);
+        }
+
+        private void bfs(string beginWord, string endWord, HashSet<string> dict,
+            Dictionary<string, List<string>> neighbours, Dictionary<string, int> distance)
         {
             var queue = new Queue<string>();
             queue.Enqueue(beginWord);
@@ -32,29 +71,36 @@ namespace WordLadder
 
             while (queue.Count > 0)
             {
-                var curr = queue.Dequeue();
-                var currDistance = distance[curr];
-                var currentNeighbours = GetNeighbours(curr, dict);
+                var count = queue.Count;
                 bool foundEnd = false;
 
-                foreach (var currentNeighbour in currentNeighbours)
+                for (int i = 0; i < count; i++)
                 {
-                    neighbours[curr].Add(currentNeighbour);
-                    if (!distance.ContainsKey(currentNeighbour))
+                    var curr = queue.Dequeue();
+                    var currDistance = distance[curr];
+                    var currentNeighbours = GetNeighbours(curr, dict);
+                    foreach (var currentNeighbour in currentNeighbours)
                     {
-                        distance.Add(currentNeighbour,0);
-                        if (currentNeighbour == endWord)
+                        neighbours[curr].Add(currentNeighbour);
+                        if (!distance.ContainsKey(currentNeighbour))
                         {
-                            foundEnd = true;
-                        }
-                        else
-                        {
-                            queue.Enqueue(currentNeighbour);
+                            distance.Add(currentNeighbour, currDistance + 1);
+                            if (currentNeighbour == endWord)
+                            {
+                                foundEnd = true;
+                            }
+                            else
+                            {
+                                queue.Enqueue(currentNeighbour);
+                            }
                         }
                     }
+
+                    if (foundEnd)
+                    {
+                        break;
+                    }
                 }
-                
-                
             }
         }
 
