27190071fc46aa9eab81b55ca7ace19e2c1dc20f
sagar <sagar.sahay@spektrix.com>
2020-04-07 21:30:35 +0100

    Diameter of binary tree

--- DiameterOfBinaryTree.Tests/SolutionTests.cs
+++ DiameterOfBinaryTree.Tests/SolutionTests.cs
@@ -1,7 +1,32 @@
+using FluentAssertions;
+using ValidateBinaryTree;
+using Xunit;
+
 namespace DiameterOfBinaryTree.Tests
 {
     public class SolutionTests
     {
-        
+        [Fact]
+        public void DiameterOfBinaryTree_ReturnsDiameter()
+        {
+            // Arrange
+            var input = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(4),
+                    right = new TreeNode(5)
+                },
+                right = new TreeNode(3)
+            };
+            
+            var sut = new Solution();
+            
+            // Act 
+            var res = sut.DiameterOfBinaryTree(input);
+            
+            // Assert
+            res.Should().Be(3);
+        }
     }
 }
--- DiameterOfBinaryTree/Solution.cs
+++ DiameterOfBinaryTree/Solution.cs
@@ -1,11 +1,42 @@
+using System;
 using ValidateBinaryTree;
 
 namespace DiameterOfBinaryTree
 {
     public class Solution
     {
-        public int DiameterOfBinaryTree(TreeNode root) {
-        
+        public int DiameterOfBinaryTree(TreeNode root) 
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var leftHeight = getHeight(root.left);
+            var rightHeight = getHeight(root.right);
+
+            var leftDiameter = DiameterOfBinaryTree(root.left);
+            var rightDiameter = DiameterOfBinaryTree(root.right);
+
+            return Math.Max(1 + leftHeight + rightHeight, Math.Max(leftDiameter, rightDiameter)) -1;
+        }
+
+        private int getHeight(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var left = getHeight(root.left);
+            var right = getHeight(root.right);
+
+            if (left > right)
+            {
+                return 1 + left;
+            }
+
+            return 1 + right;
         }
     }
 }
